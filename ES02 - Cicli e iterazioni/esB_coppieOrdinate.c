5f2b5943049ea4eb4efed84b505553f05c5b540a
alessandro santoro <113691739+alessandro-santoro@users.noreply.github.com>
2022-12-02 21:03:03 +0100

    Add files via upload

---
+++ ES02 - Cicli e iterazioni/esB_coppieOrdinate.c
@@ -0,0 +1,31 @@
+/** ****************************************************************************************
+* \mainpage esB_coppieOrdinate.c
+*
+* @brief Scrivere un programma che visualizzi tutte le coppie ordinate di numeri naturali 
+*        la cui somma è 15. 
+* 
+* @author alessandro santoro
+* @date 02/12/2022
+*/
+
+#include <stdio.h>      
+
+int main() 
+{
+	int c,co;           
+	
+	
+    for( c = 1; c < 8; c++)              
+		
+		
+        for( co = 1; co < 15; co++ )         
+			
+			
+            if( c + co == 15 ) 
+					                    
+                	printf("%d + %d \n", c, co);
+            	
+                
+                
+                
+}
