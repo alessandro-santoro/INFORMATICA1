72d805a37469d3a74cf8260b0e13cfc96cf82c2f
alessandro santoro <113691739+alessandro-santoro@users.noreply.github.com>
2022-12-03 23:05:59 +0100

    Add files via upload

---
+++ ES02 - Cicli e iterazioni/esC_media.c
@@ -0,0 +1,35 @@
+/** ****************************************************************************************
+* \mainpage esC_media.c
+*
+* @brief Si scriva un programma per calcolare la media aritmetica di una serie di numeri 
+*        inseriti da tastiera. L’introduzione del valore 0 indica il termine del caricamento 
+*        dei dati.
+* 
+* @author alessandro santoro
+* @date 03/12/2022
+*/
+
+#include <stdio.h>      
+
+int main() 
+{
+
+	int num, c=0, somma=0;       
+    float media=0;             
+
+    do 
+    {                                         
+        printf("Insersici un numero: ");
+        scanf("%d", &num);
+
+        media += num;
+
+        if(num!=0) 
+            c++;                                
+    } while (num!=0);                         
+
+    media /= c;             
+
+    printf("\nLa media dei numeri e' %.2f\n", media);
+}
+
