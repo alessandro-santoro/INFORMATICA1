f6401b53ff9825c5a11bed36fa4a31c95fea9ab1
alessandro santoro <113691739+alessandro-santoro@users.noreply.github.com>
2022-12-09 19:28:42 +0100

    Update esA_divisore.c

--- ES02 - Cicli e iterazioni/esA_divisore.c
+++ ES02 - Cicli e iterazioni/esA_divisore.c
@@ -7,18 +7,18 @@
 * @date 01/12/2022
 */
 
-#include <stdio.h>
+#include <stdio.h>    //librerie
 
 int main() 
 {
 	int num, d;
 	
-	printf("Inserisci un numero: ");
+	printf("Inserisci un numero: ");   // chiedo un numero/valore all'utente
 	scanf("%d", &num);
 	
-	for(d = 2; d <= num/2; d++)
+	for(d = 2; d <= num/2; d++)     //itero tutti gli interi da 2 al numero inserito
 	{
 		if(num%d==0)
-			printf("%d e' un divisore\n", d);
+			printf("%d e' un divisore\n", d);     //controllo se l'indice e' un divisore
 	}	
 }
