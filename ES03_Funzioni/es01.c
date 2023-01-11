9e3d58664818651a195986030f6eaaa3924b6841
alessandro santoro <ale004santoro@gmail.com>
2023-01-11 12:31:23 +0000

    funzioni

---
+++ ES03_Funzioni/es01.c
@@ -0,0 +1,21 @@
+/** ****************************************************************************************
+* \mainpage es01.c
+*
+* @brief programma per la somma di dieci numeri.
+* 
+* @author alessanro santoro
+* @date 11/01/2023
+*/
+
+#include<stdio.h>
+int somma(num,num);
+
+int main()
+{
+    int r=0;
+    int num;
+    int c;
+    
+
+
+}
