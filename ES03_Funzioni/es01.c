0528cfbae2497309e1ed3e47b36f14c9c2f934b8
alessandro santoro <ale004santoro@gmail.com>
2023-01-25 12:53:41 +0000

    update

--- ES03_Funzioni/es01.c
+++ ES03_Funzioni/es01.c
@@ -2,68 +2,165 @@
 * \mainpage funzioni 
 *
 * @brief 
+* 1) Scrivere una funzione chiamata "mcd" che prende in input due numeri interi e restituisce 
+*    il loro massimo comune divisore utilizzando l'algoritmo di Euclide. 
+* 2) Scrivere una funzione chiamata "somma_divisori" che prende in input un numero intero 
+*    e restituisce la somma dei suoi divisori.
+* 3) Scrivere una funzione chiamata "area_rettangolo" che prende in input la base e 
+*    l'altezza di un rettangolo e restituisce l'area. 
 * 
-* @author Alessandro Santoro
-* @date 11/01/2023
+* @author alessandro santoro
+* @date 13/01/2023
 * @version 1.0 versione iniziale
 */
 
 #include<stdio.h>
 #include<limits.h>
+#include<stdlib.h>
+
+/** ****************************************************************************************
+* @brief menu che chiede ad inizio programma quale funzione usare
+* @param nessuno
+* @retval la scelta dell'utente
+*
+* @author Alessandro Santoro
+* @version 1.0 13/01/2023 versione iniziale
+*/
+int menu();
 
 
-int med(int,int,int);       //restituisce il massimo comume divisore di due numeri 
+/** ****************************************************************************************
+* @brief restituisce il massimo comume divisore di due numeri
+* @param n1 primo numero
+* @param n2 secondo numero
+* @retval l' MCD di n1 e n2
+*
+* @author Alessandro Santoro
+* @version 1.0 13/01/2023 versione iniziale
+*/
+int mcd(int,int,int);
 
 
-int somma_divisori(int);        //restituisce la somma dei divisori di un numero inserito da tastiera
+
+/** ****************************************************************************************
+* @brief restituisce la somma dei divisori di un numero inserito da tastiera
+* @param n3
+* @retval somma dei divisori
+*
+* @author Alessandro Santoro
+* @version 1.0 13/01/2023 versione iniziale
+*/
+int somma_divisori(int);
 
 
-int area_rettangolo(int,int);       //restituisce l'aria di un rettangolo ricevendo in input la base e l'altezza
+/** ****************************************************************************************
+* @brief restituisce l'aria di un rettangolo ricevendo in input la base e l'altezza
+* @param base
+* @param altezza
+* @retval area di base e altezza
+*
+* @author Alessandro Santoro
+* @version 1.0 13/01/2023 versione iniziale
+*/
+
+int area_rettangolo(int,int);
 
 int main()
 {
-    printf("alessandro santoro \n");
+    int n1,n2,ris=0,r;
+   
 
-    int num1,num2,ris=0,mcd;
-    int num3,somma;
-    int base,altezza,area;
-
-    printf("inserisci il primo numero ");
-    scanf("%d",&num1);
-    printf("inserisci il secondo numero ");
-    scanf("%d",&num2); 
-    mcd=med(num1,num2,ris);
-    printf("il massimo comune divisore e: %d\n",mcd);
-
-    printf("\ninserisci un numero dal quale verranno sommati i divisori ");
-    scanf("%d",&num3);
-    somma=somma_divisori(num3);
-    printf("la somma dei divisori di %d e %d \n",num3,somma);
-
-    printf("\niserisci la base del rettangolo ");
-    scanf("%d",&base);
-    printf("iserisci l'altezza del rettangolo ");
-    scanf("%d",&altezza);
-    area=area_rettangolo(base,altezza);
-    printf("l'area del rettangolo e %d\n",area);
-
+    do
+    {
+        r = menu();
+        switch(r)
+        {
+            case 0: printf("\n Fine programma \n");
+            
+                    break;
+                   
+                   
+            case 1: printf(" hai scelto mcd \n");
+            
+					printf("\n inserisci il primo numero: ");
+                    scanf("%d",&n1);
+                    
+                    printf("\n inserisci il secondo numero: ");
+                    scanf("%d",&n2); 
+                    
+                   ris = mcd ( n1 , n2 , ris );
+                    
+                    printf("il massimo comune divisore e: %d\n",ris);
+                    
+                    break;
+                    
+                    
+            case 2: printf("\nhai scelto somma divisori");
+            
+					printf("\ninserisci un numero dal quale verranno sommati i divisori:  ");
+                    scanf("%d",&n1);
+                    
+                    ris = somma_divisori ( n1 );
+                    
+                    printf("la somma dei divisori di %d e %d \n",n1,ris); 
+                    
+                    break;
+                    
+            case 3: printf("\n hai scelto area rettangolo");
+					printf("\ninserisci la base del rettangolo:  ");
+                    scanf("%d",&n1);
+                    
+                    printf("inserisci l'altezza del rettangolo:  ");
+                    scanf("%d",&n2);
+                    
+                    ris = area_rettangolo ( n1 , n2 );
+                    
+                    printf("l'area del rettangolo e %d\n",ris);  
+                    
+                    break;
+                    
+                    
+            default: printf("hai inserito un numero sbagliato\n");     
+			
+			                      
+        }
+        //system("pause");
+        getchar();
+        getchar();
+    }
+    while(r!=0);
+   
     return 0;
 }
 
-int med(int num1,int num2,int ris)
+int menu()
 {
-    while (num2 != 0) 
+	system("clear");
+    int risp;
+    printf("\n\n alessandro santoro \n");
+    printf("\n digita 0 per uscire dal programma\n");
+    printf("\n digita 1 per calcolare l'mcd tra due numeri interi\n");
+    printf("\n digita 2 per calcolare la somma tra i divisori du un numero intero\n");
+    printf("\n digita 3 per calcolare l'area di un rettangolo con base e altezza ineri\n");
+    printf(" inserisci:  ");
+    scanf("%d",&risp);
+    return risp;
+}
+
+int mcd(int n1,int n2,int ris)
+{
+    while (n2 != 0) 
     {
-        ris = num2;
-        num2 = num1 % num2;
-        num1 = ris;
+        ris = n2;
+        n2 = n1 % n2;
+        n1 = ris;
     }
-    return num1;
+    return n1;
 }
 
 int somma_divisori(int num3)
 {
-    int somma,cont;
+    int ris,cont;
     for(cont=1;cont<=num3;cont++)
     
         if(num3%cont==0)
