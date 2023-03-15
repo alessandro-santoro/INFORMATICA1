f71fc56b550e7723afe6aff011aa4fbdba91c558
alessandro santoro <ale004santoro@gmail.com>
2023-03-15 12:53:31 +0000

    vettore.c

--- ES03_Funzioni/vettori.c
+++ ES03_Funzioni/vettori.c
@@ -5,28 +5,33 @@
 #include <stdlib.h>
 
 #define n 10
-
+/*
 void scambio(int x, int y);
-
+*/
 void carica(int[]);
-
+/*
 void caricaRand(int []);
-
+*/
 void stampovett(int []);
-
+/*
 void cerca_num(int x,int []);
 
 void cerca_num_ris(int x, int v[]);
 
 int contamultipli_vettori(int [], int x);
 
+void ribaltavett(int v[]);*/
+
+void copiavett(int v1[],int v2[]);
+
 int main()
 {
-    int vettore[n];
+    int vettore[n], vettore2[n];
     int r,num;
 
+    
     carica(vettore);
-
+/*
     caricaRand(vettore);
 
     stampovett(vettore);
@@ -37,8 +42,14 @@ int main()
 
     printf("%d\n",r);
 
+    ribaltavett(vettore);*/
+
+    copiavett(vettore,vettore2);
+    
+
 
 }
+/*
 
 void scambio(int x, int y)
 {
@@ -49,7 +60,7 @@ void scambio(int x, int y)
     y = scambio;
 
 }
-
+*/
 void carica(int v[ ])
 {
     int c;
@@ -59,8 +70,10 @@ void carica(int v[ ])
         printf("\ninserisci un nuemro\n");
         scanf("%d",& v[c]);
     }
-}
 
+    stampovett(v);
+}
+/*
 void caricaRand(int v[])
 {
     srand(time(NULL));
@@ -68,8 +81,10 @@ void caricaRand(int v[])
     {
         v[c] = rand()%50 + 1;
     }
-}
 
+    stampovett(v);
+}
+*/
 void stampovett(int v[])
 {
     for(int c = 0; c<n; c++)
@@ -78,7 +93,7 @@ void stampovett(int v[])
     }
     printf("\n\n");
 }
-
+/*
 void cerca_num(int x, int v[])
 {
     for(int c=0;c<n;c++)
@@ -106,9 +121,33 @@ int contamultipli_vettori(int v[], int x)
 
     for(int i =0; i<n; i++)
     {
-        if(v[i]x==0)
+        if(v[i]%x==0)
         {
             c++;
         }
     }return c;
+}
+
+void ribaltavett(int v[])
+{
+    int c,j,x;
+
+    for(c=0,j=n-1;c<=n/2;c++,j--)
+    {
+        x=v[c];
+        v[c]=v[j];
+        v[j]=x;
+    }
+
+    stampovett(v);
+}
+*/
+void copiavett(int v1[],int v2[])
+{
+    for(int c=0;c<n;c++)
+    {
+        v2[c]=v1[c];
+    }
+
+    stampovett(v2);
 }
