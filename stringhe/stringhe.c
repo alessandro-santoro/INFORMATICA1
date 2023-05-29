8600f3ac046b348bc79152efb4be6650e1ceb1fe
alessandro santoro <ale004santoro@gmail.com>
2023-05-29 07:43:39 +0000

    stringhe.c

--- stringhe/stringhe.c
+++ stringhe/stringhe.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-#define n 10000
+#define n 1000
 
 int main() 
 {
     char str1[n], str2[n];
-    int l, a;
+    int a, b;
 
     scanf("%s", str1);
     scanf("%s", str2);
     printf("%s %s", str1, str2);
 
 
-    l = strlen(str1);
-    printf("\n\n%d\n\n", l);
+    a = strlen(str1);
+    printf("\n\n%d\n\n", a);
 
     strcpy(str1, str2);
     printf("1: %s\n\n2: %s", str1, str2);
@@ -22,7 +22,7 @@ int main()
     strcat(str1, str2);
     printf("\n\n%s\n\n", str1);
 
-    a = strcmp(str1,str2);
-    printf("%d", a);
+    b = strcmp(str1,str2);
+    printf("%d", b);
 
 }
