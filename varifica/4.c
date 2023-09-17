1cb5af1b0c005114d28c8a1008519a817e75af57
alessandro santoro <ale004santoro@gmail.com>
2023-09-17 09:05:03 +0000

    4.c

--- varifica/4.c
+++ varifica/4.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define C 5
-#define R 5
+# define N 10
+# define C 5
+# define R 5
 
 int menu(void);
 
@@ -15,11 +16,20 @@ void stampavett(int v[]);
 
 void nuovoInserisci(int v[],int x);
 
+int conta(int v[], int x);
+
+void causualimat(int m[][C]);
+
+void stampamat(int m[][C]);
+
+void dispari(int m[][C], int y);
+
 int main()
 {
     srand(time(NULL));
 
-    int vett[C];
+    int vett[N];
+    int matrice[R][C];
     int r;
     int n,n2;
     int k;
@@ -63,6 +73,46 @@ int main()
                         nuovoInserisci(vett,n);
                 break;
 
+                case 4:
+                        printf("\n conta\n");
+
+                        printf("\n inserisci un numero:  ");
+                        scanf("%d",&n);
+
+                        r=conta(vett,n);
+
+                        stampavett(vett);
+
+                        printf("\n");
+
+                        printf("\n%d",r);
+                break;
+
+                case 5:
+                        printf("\n casuali matrice\n");
+
+                        causualimat(matrice);
+
+                        stampamat(matrice);
+                break;
+
+                case 6:
+                        printf("\n stampa matrice\n");
+
+                        stampamat(matrice);
+                break;
+
+                case 7:
+                        printf("\n dispari matrice\n");
+
+                        printf("\n inserisci un numero:  ");
+                        scanf("%d",&n);
+
+                        dispari(matrice,n);
+
+                        stampamat(matrice);
+                break;
+
                 default : printf(" hai sbagliato numero");     
 
 
@@ -85,6 +135,10 @@ int menu(void)
     printf("\n 1 carica rand");
     printf("\n 2 carica");
     printf("\n 3 nuovo insersci");
+    printf("\n 4 conta");
+    printf("\n 5 casuali matrice");
+    printf("\n 6 stampa matrice");
+    printf("\n 7 dispari");
 
     scanf("%d",&risp);
 
@@ -93,7 +147,7 @@ int menu(void)
 
 void caricarand(int v[])
 {
-    for(int i=0;i<C;i++)
+    for(int i=0;i<N;i++)
     {
         v[i] = rand() %41+10;
     }
@@ -107,7 +161,7 @@ void carica(int v[], int x)
     {
         x++;
     }
-    for(int i=0;i<C;i++)
+    for(int i=0;i<N;i++)
     {
         v[i]=x;
         x+=2;
@@ -118,7 +172,7 @@ void carica(int v[], int x)
 
 void stampavett(int v[])
 {
-    for(int i=0;i<C;i++)
+    for(int i=0;i<N;i++)
     {
         printf("\t%d",v[i]);
     }
@@ -128,9 +182,62 @@ void nuovoInserisci(int v[],int x)
 {
     int c=0;
     v[0]=x;
-    for(int i=0;i<C;i++)
+    for(int i=0;i<N;i++)
     {
 
     }
     stampavett(v);
+}
+
+int conta(int v[], int x)
+{
+    int c=0;
+    for(int i=0;i<N;i++)
+    {
+        if(v[i]>x)
+        {
+            c++;
+        }
+    }
+    return c;
+}
+
+void causualimat(int m[][C])
+{
+    for(int i=0;i<R;i++)
+    {
+        for(int j=0;j<C;j++)
+        {
+            m[i][j] = rand() %41+10;
+        }
+    }
+}
+
+void stampamat(int m[][C])
+{
+    for(int i=0;i<R;i++)
+    {
+        printf("\n");
+
+        for(int j=0;j<C;j++)
+        {
+            printf("\t%d",m[i][j]);
+        }
+    }
+}
+
+void dispari(int m[][C], int y)
+{
+    for(int i=0;i<C;i++)
+    {
+        printf("\n");
+
+        for(int j=0;j<R;j++)
+        {
+            if(m[i][j]%2==1)
+            {
+                m[i][j]=y;
+            }
+        }
+    }
 }
