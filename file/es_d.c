3a22ace76ef06afa67d4377bc108e3aee51b4491
alessandro santoro <ale004santoro@gmail.com>
2023-10-24 08:02:46 +0000

    file.c

--- file/es_d.c
+++ file/es_d.c
@@ -10,9 +10,12 @@
 void copia(char filein[], char fileout[]); /*Scrivi  un  programma  che  esegue  la  copia  di  un  file  di  testo.  
 Il  programma  legge  carattere per carattere il contenuto del file in.txt e lo ricopia nel file out.txt. */
 
+void Maiuscolo(char filein[], char fileout[]);
+
 int main()
 {
     copia("input.txt", "output.txt");
+    Maiuscolo("input.txt","output.txt");
 }
 
 void copia(char filein[], char fileout[])
@@ -32,4 +35,27 @@ void copia(char filein[], char fileout[])
 
     fclose(input);
     fclose(output);
+}
+
+void Maiuscolo(char filein[], char fileout[])
+{
+    char x;
+
+    FILE * minuscolo = fopen(filein, "r");
+    FILE * maiuscolo = fopen(fileout, "w");
+    
+    while(!feof(minuscolo))
+    {
+        x = getc(minuscolo);
+
+        if(x >= 'a' && x <= 'z')
+        {
+            x-=32;
+        }    
+
+        putc(x, minuscolo);
+    } 
+
+    fclose(minuscolo);
+    fclose(maiuscolo);
 }
