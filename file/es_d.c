7c3256d0b9fcf341a31842c4bed8d526f1a624e2
alessandro santoro <ale004santoro@gmail.com>
2023-10-25 12:02:23 +0000

    file.c

--- file/es_d.c
+++ file/es_d.c
@@ -6,16 +6,23 @@
 */
 
 #include <stdio.h>
+int lettere = 0;
+int parole = 1; 
+int righe = 1;
 
 void copia(char filein[], char fileout[]); /*Scrivi  un  programma  che  esegue  la  copia  di  un  file  di  testo.  
 Il  programma  legge  carattere per carattere il contenuto del file in.txt e lo ricopia nel file out.txt. */
 
-void Maiuscolo(char filein[], char fileout[]);
+void maiuscolo(char filein[], char fileout[]);
+
+void conta_l_p_r(char file[]);
 
 int main()
 {
     copia("input.txt", "output.txt");
-    Maiuscolo("input.txt","output.txt");
+    maiuscolo("input.txt","output.txt");
+    conta_l_p_r("input.txt");
+    printf("\n le lettere sono:\t%d\n le parole sono:\t%d\n le righe sono:\t%d\n",lettere,parole,righe);
 }
 
 void copia(char filein[], char fileout[])
@@ -37,25 +44,52 @@ void copia(char filein[], char fileout[])
     fclose(output);
 }
 
-void Maiuscolo(char filein[], char fileout[])
+void maiuscolo(char filein[], char fileout[])
 {
     char x;
 
-    FILE * minuscolo = fopen(filein, "r");
-    FILE * maiuscolo = fopen(fileout, "w");
+    FILE * minusc = fopen(filein, "r");
+    FILE * maiusc = fopen(fileout, "w");
     
-    while(!feof(minuscolo))
+    while(!feof(minusc))
     {
-        x = getc(minuscolo);
+        x = getc(minusc);
 
-        if(x >= 'a' && x <= 'z')
+        if(x >= 'a' && x <= 'z')   
         {
-            x-=32;
-        }    
-
-        putc(x, minuscolo);
+             x-=32;
+        }
+        putc(x, maiusc);
     } 
 
-    fclose(minuscolo);
-    fclose(maiuscolo);
+    fclose(minusc);
+    fclose(maiusc);
+}
+
+void conta_l_p_r(char filein[])
+{
+    FILE * file = fopen(filein, "r");
+
+    char c;
+
+    while(!feof(file))
+    {
+        c = getc(file);
+
+        if(c != ' ' && c != '\n')
+        {
+            lettere++;
+        }
+        else if(c == ' ')
+        {
+            parole++;
+        }
+        else if(c == '\n') 
+        {
+            righe++; 
+            parole++;
+        }
+    }
+
+    fclose(file);
 }
