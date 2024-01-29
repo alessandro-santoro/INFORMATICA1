7f3612dce8da8415f47c22ad0147114bc6545208
alessandro santoro <ale004santoro@gmail.com>
2024-01-29 08:59:59 +0000

    recupero

--- recupero/primo.c
+++ recupero/primo.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-#define N 3
+#define N 2
+
+struct data
+{
+	int giorno;
+	int mese;
+	int anni;
+}typedef data;
 
 struct studente
 {
@@ -11,13 +18,6 @@ struct studente
 	int anno;
 	data nascita;
 }typedef studente;
-
-struct data
-{
-	int giorno;
-	int mese;
-	int anni;
-}typedef data;
 
 void inserisciDati(	studente s[]);
 void stampaDati(studente s[]);
@@ -35,31 +35,31 @@ void inserisciDati(	studente s[])
 	
 	for(int i=0;i<N;i++)
 	{
-		printf("inserisci nome:\t");
+		printf("\ninserisci nome:\t");
 		scanf("%s",s[i].nome);
 		
-		printf("inserisci cognome:\t");
+		printf("\ninserisci cognome:\t");
 		scanf("%s",s[i].cog);
 		
-		printf("inserisci sezione:\t");
+		printf("\ninserisci sezione:\t");
 		scanf("%s",s[i].sezione);
 		
-		printf("inserisci anno:\t");
-		scanf("%d",s[i].anno);
+		printf("\ninserisci anno:\t");
+		scanf("%d",&s[i].anno);
 		
-		printf("inserisci giorno di nasctita:\t");
-		scanf("%d",s[i].nascita.giorno);
+		printf("\ninserisci giorno di nasctita:\t");
+		scanf("%d",&s[i].nascita.giorno);
 		
-		printf("inserisci mese di nasctita:\t");
-		scanf("%d",s[i].nascita.mese);
+		printf("\ninserisci mese di nasctita:\t");
+		scanf("%d",&s[i].nascita.mese);
 		
-		printf("inserisci anno di nasctita:\t");
-		scanf("%d",s[i].nascita.anni);
+		printf("\ninserisci anno di nasctita:\t");
+		scanf("%d",&s[i].nascita.anni);
 		
 		for(int c=0;c<10;c++)
 		{
-			printf("inserisci il %d° voto:\t",c+1);
-			scanf("%d",s[i].voto[c]);
+			printf("\ninserisci il %d° voto:\t",c+1);
+			scanf("%d",&s[i].voto[c]);
 		}
 	}
 }
@@ -68,16 +68,16 @@ void stampaDati(studente s[])
 {
 	for(int i=0;i<N;i++)
 	{
-		printf("nome:\t%s",s[i].nome);
-		printf("cognome:\t%s",s[i].cog);
-		printf("sezione:\t%s",s[i].sezione);
-		printf("anno:\t%d",s[i].anno);
-		printf("gionro di nascita:\t",s[i].nascita.giorno);
-		printf("mese di nascita:\t",s[i].nascita.mese);
-		printf("anno dui nascita:\t",s[i].nascita.anni);
+		printf("\nnome:\t%s",s[i].nome);
+		printf("\ncognome:\t%s",s[i].cog);
+		printf("\nsezione:\t%s",s[i].sezione);
+		printf("\nanno:\t%d",s[i].anno);
+		printf("\ngionro di nascita:\t%d",s[i].nascita.giorno);
+		printf("\nmese di nascita:\t%d",s[i].nascita.mese);
+		printf("\nanno dui nascita:\t%d\n",s[i].nascita.anni);
 		for(int c=0;c<10;c++)
 		{
-			printf("%d° voto:\t%d",c+1,s[i].voto[c]);
+			printf("\t%d° voto:\t%d",c+1,s[i].voto[c]);
 		}
 	}
 }
